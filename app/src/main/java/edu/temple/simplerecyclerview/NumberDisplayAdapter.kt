db9d83b018b9940fbd894346d59a05a25317e737
njohns-tmpl <tuq84855@temple.edu>
2026-02-12 11:56:37 -0500

    Resolved todos for in class activity 2/12

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -6,20 +6,19 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-// TODO(Step 1: Implement click behavior)
+class NumberDisplayAdapter(private val numbers: Array<Int>, private val callback: (Int) -> Unit) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
-// TODO (Step 1a: Accept lambda in constructor)
-class NumberDisplayAdapter (private val numbers: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
+    inner class NumberViewHolder(layout: View) : RecyclerView.ViewHolder(layout) {
+        val textView: TextView = layout.findViewById(R.id.textView)
 
-    // TODO (Step 1b: Invoke lambda via onClickListener)
-    inner class NumberViewHolder (layout: View) : RecyclerView.ViewHolder (layout) {
-        // enumerate views inside layout
-        val textView = layout.findViewById<TextView>(R.id.textView)
-
+        init {
+            textView.setOnClickListener {
+                callback(numbers[adapterPosition])
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
-
         val layout = LayoutInflater.from(parent.context).inflate(R.layout.textview_layout, parent, false)
         return NumberViewHolder(layout)
     }
